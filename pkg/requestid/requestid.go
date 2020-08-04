393fdb71e5953d537d0ab293619153618859c1f4
Matteo Giaccone <matteo.giaccone@vonage.com>
2020-08-04 17:06:41 +0100

    [NUAPI-349] Handle request ID helpers

---
+++ pkg/requestid/requestid.go
@@ -0,0 +1,35 @@
+package requestid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/nexmoinc/gosrvlib/pkg/httputil"
+)
+
+const (
+	headerRequestID = "X-Request-Id"
+)
+
+type ctxKey struct{}
+
+// FromContext retrieves the request ID from the context. If not found the defaultValue is returned instead
+func FromContext(ctx context.Context, defaultValue string) string {
+	if l, ok := ctx.Value(ctxKey{}).(string); ok {
+		return l
+	}
+	return defaultValue
+}
+
+// WithRequestID builds a new context with the request ID to the given parent context
+func WithRequestID(ctx context.Context, id string) context.Context {
+	if _, ok := ctx.Value(ctxKey{}).(string); ok {
+		return ctx
+	}
+	return context.WithValue(ctx, ctxKey{}, id)
+}
+
+// FromHTTPRequest retrieves the request ID from a http.Request. If not found the defaultValue is returned instead
+func FromHTTPRequest(r *http.Request, defaultValue string) string {
+	return httputil.HeaderOrDefault(r, headerRequestID, defaultValue)
+}
---
+++ pkg/requestid/requestid_test.go
@@ -0,0 +1,57 @@
+package requestid
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFromContext(t *testing.T) {
+	t.Parallel()
+
+	// Context without request id, should return given default
+	id1 := FromContext(context.Background(), "default-12345")
+	require.NotEmpty(t, id1)
+	require.Equal(t, "default-12345", id1)
+
+	// Context with request id, should return the existing value
+	ctx := WithRequestID(context.Background(), "context-12345")
+	id2 := FromContext(ctx, "default-67890")
+	require.NotEmpty(t, id2)
+	require.Equal(t, "context-12345", id2)
+}
+
+func TestWithRequestID(t *testing.T) {
+	t.Parallel()
+
+	ctx := WithRequestID(context.Background(), "test-123456")
+
+	el1 := FromContext(ctx, "default-123456")
+	require.Equal(t, el1, "test-123456")
+
+	// do not override with the same id
+	ctx1 := WithRequestID(ctx, "test-123456")
+	require.Equal(t, ctx, ctx1)
+
+	// do not override with other id
+	ctx2 := WithRequestID(ctx, "other-123456")
+	require.Equal(t, ctx, ctx2)
+}
+
+func TestFromHTTPRequest(t *testing.T) {
+	t.Parallel()
+
+	// header not set should return default
+	r1, _ := http.NewRequest(http.MethodGet, "/", nil)
+	v1 := FromHTTPRequest(r1, "default-123456")
+	require.Equal(t, "default-123456", v1)
+
+	// header set should return actual value
+	r2, _ := http.NewRequest(http.MethodGet, "/", nil)
+	r2.Header.Add(headerRequestID, "reqid-1234565789")
+
+	v2 := FromHTTPRequest(r2, "default-123456")
+	require.Equal(t, "reqid-1234565789", v2)
+}
