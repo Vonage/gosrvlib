8b765736058b761e5721b779a08715693bdeccea
Nicola Asuni <nicolaasuni@users.noreply.github.com>
2020-08-28 17:02:13 +0100

    Cleanup example (#19)

--- examples/service/internal/cli/bind.go
+++ examples/service/internal/cli/bind.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/nexmoinc/gosrvlib-sample-service/internal/httphandler"
 	"github.com/nexmoinc/gosrvlib/pkg/bootstrap"
+	"github.com/nexmoinc/gosrvlib/pkg/healthcheck"
 	"github.com/nexmoinc/gosrvlib/pkg/httpserver"
 	"github.com/nexmoinc/gosrvlib/pkg/httputil/jsendx"
 	"go.uber.org/zap"
@@ -14,11 +15,7 @@ import (
 // bind is the entry point of the service, this is where the wiring of all components happens
 func bind(cfg *appConfig, appInfo *jsendx.AppInfo) bootstrap.BindFunc {
 	return func(ctx context.Context, l *zap.Logger) error {
-		defaultServerOpts := []httpserver.Option{
-			httpserver.WithRoutesIndexHandlerFunc(jsendx.DefaultRoutesIndexHandler(appInfo)),
-			httpserver.WithPingHandlerFunc(jsendx.DefaultPingHandler(appInfo)),
-		}
-		statusHandler := httpserver.WithStatusHandlerFunc(jsendx.DefaultStatusHandler(appInfo))
+		var statusHandler httpserver.Option
 
 		// We assume the service is disabled and override the service binder if required
 		serviceBinder := httpserver.NopBinder()
@@ -29,30 +26,25 @@ func bind(cfg *appConfig, appInfo *jsendx.AppInfo) bootstrap.BindFunc {
 			//
 			serviceBinder = httphandler.New(nil)
 
-			// NOTE: Uncomment the following block use a custom healthcheck handler
-			//
-			// // NOTE: Uncomment to create a custom healthcheck handler with default response
-			// // healthCheckHandler := healthcheck.NewHandler(
-			// // 	[]healthcheck.HealthCheck{
-			// // 		healthcheck.New("<ID>", < HANDLER >),
-			// // 		healthcheck.NewWithTimeout("<ID>", <HANDLER>, <TIMEOUT>),
-			// // 	},
-			// // )
-			//
-			// // NOTE: Uncomment to create a custom healthcheck handler with JSendX response
-			// // healthCheckHandler := healthcheck.NewHandler(
-			// // 	[]healthcheck.HealthCheck{
-			// // 		healthcheck.New("<ID>", < HANDLER >),
-			// // 		healthcheck.NewWithTimeout("<ID>", <HANDLER>, <TIMEOUT>),
-			// // 	},
-			// // 	healthcheck.WithResultWriter(jsendx.HealthCheckResultWriter(appInfo)),
-			// // )
-			//
-			// // override the default healthcheck handler
-			// statusHandler = httpserver.WithStatusHandlerFunc(healthCheckHandler.ServeHTTP)
+			// Custom healthcheck handler with JSendX response
+			healthCheckHandler := healthcheck.NewHandler(
+				[]healthcheck.HealthCheck{
+					// healthcheck.New("<ID>", < HANDLER >),
+					// healthcheck.NewWithTimeout("<ID>", <HANDLER>, <TIMEOUT>),
+				},
+				healthcheck.WithResultWriter(jsendx.HealthCheckResultWriter(appInfo)),
+			)
+			statusHandler = httpserver.WithStatusHandlerFunc(healthCheckHandler.ServeHTTP)
+		} else {
+			statusHandler = httpserver.WithStatusHandlerFunc(jsendx.DefaultStatusHandler(appInfo))
 		}
 
-		defaultServerOpts = append(defaultServerOpts, statusHandler)
+		defaultServerOpts := []httpserver.Option{
+			httpserver.WithRoutesIndexHandlerFunc(jsendx.DefaultRoutesIndexHandler(appInfo)),
+			httpserver.WithPingHandlerFunc(jsendx.DefaultPingHandler(appInfo)),
+			statusHandler,
+		}
+
 		httpServiceOpts := append(defaultServerOpts, httpserver.WithServerAddr(cfg.ServerAddress))
 
 		// Use a separate server for monitoring routes if monitor_address and server_address are different
