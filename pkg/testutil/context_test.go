d3d1fb4b914cc7ad9f2c5e25c99bbb6673c5c687
Matteo Giaccone <matteo.giaccone@vonage.com>
2020-09-07 16:49:03 +0100

    [NUAPI-415] Add coverage to testutil package

---
+++ pkg/testutil/context_test.go
@@ -0,0 +1,41 @@
+package testutil
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/nexmoinc/gosrvlib/pkg/httputil"
+	"github.com/nexmoinc/gosrvlib/pkg/logging"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestContext(t *testing.T) {
+	ctx := Context()
+
+	l1 := logging.FromContext(ctx)
+	l2 := logging.FromContext(ctx)
+	require.Equal(t, l1, l2)
+}
+
+func TestContextWithLogObserver(t *testing.T) {
+	ctx, logs := ContextWithLogObserver(zap.DebugLevel)
+
+	l := logging.FromContext(ctx)
+	l.Info("test message")
+
+	require.Equal(t, 1, logs.Len())
+	require.Equal(t, "test message", logs.All()[0].Message)
+}
+
+func TestContextWithHTTPRouterParams(t *testing.T) {
+	params := map[string]string{"test_arg_1": "test_val_1", "test_arg_2": "test_val_2"}
+
+	r := httptest.NewRequest(http.MethodGet, "http://test.url.invalid", nil)
+	r = r.WithContext(ContextWithHTTPRouterParams(context.Background(), params))
+
+	require.Equal(t, "test_val_1", httputil.PathParam(r, "test_arg_1"))
+	require.Equal(t, "test_val_2", httputil.PathParam(r, "test_arg_2"))
+}
---
+++ pkg/testutil/http_test.go
@@ -0,0 +1,28 @@
+package testutil
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/nexmoinc/gosrvlib/pkg/httputil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRouterWithHandler(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(Context(), http.MethodGet, "/test", nil)
+
+	router := RouterWithHandler(http.MethodGet, "/test", func(w http.ResponseWriter, r *http.Request) {
+		httputil.SendStatus(r.Context(), w, http.StatusOK)
+	})
+	router.ServeHTTP(rr, req)
+
+	resp := rr.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	require.Equal(t, "text/plain; charset=utf-8", resp.Header.Get("Content-Type"))
+	require.Equal(t, "OK\n", string(body))
+}
