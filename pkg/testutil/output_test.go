7eb70609c2447046f6d2e096e9061c1b39c2a8e7
Matteo Giaccone <matteo.giaccone@vonage.com>
2020-09-14 09:31:14 +0100

    [NUAPI-415] Improve test coverage (#24)
    * Add helper method to put httprouter params in a ctx
    
    * Fix formatting
    
    * Revert "Add helper method to put httprouter params in a context.Context" (#22)
    
    * [NUAPI-415] Add coverage to testutil package
    
    * Remove unused PR template
    
    Co-authored-by: Corneliu Vesa <corneliu.vesa@vonage.com>
    Co-authored-by: Nicola Asuni <nicolaasuni@users.noreply.github.com>

---
+++ pkg/testutil/output_test.go
@@ -0,0 +1,19 @@
+package testutil
+
+import (
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCaptureOutput(t *testing.T) {
+	testFn := func() {
+		log.Printf("test output capture")
+	}
+
+	output := CaptureOutput(t, testFn)
+	output = strings.TrimSuffix(output, "\n")
+	require.Regexp(t, `^[0-9]{4}(/[0-9]{2}){2}\s([0-9]{2}:){2}[0-9]{2}\stest\soutput\scapture$`, output)
+}
