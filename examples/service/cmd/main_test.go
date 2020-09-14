daac832bbded043fec0c697ab258d5e1714550b2
nicolaasuni <nicola.asuni@vonage.com>
2020-09-14 14:46:55 +0100

    Test fatal exit in main

--- examples/service/cmd/main_test.go
+++ examples/service/cmd/main_test.go
@@ -6,7 +6,10 @@ import (
 	"testing"
 
 	"github.com/nexmoinc/gosrvlib-sample-service/internal/cli"
+	"github.com/nexmoinc/gosrvlib/pkg/logging"
 	"github.com/nexmoinc/gosrvlib/pkg/testutil"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
 )
 
 func TestProgramVersion(t *testing.T) {
@@ -14,7 +17,6 @@ func TestProgramVersion(t *testing.T) {
 	out := testutil.CaptureOutput(t, func() {
 		main()
 	})
-
 	match, err := regexp.MatchString("^[\\d]+\\.[\\d]+\\.[\\d]+[\\s]*$", out)
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
@@ -22,4 +24,20 @@ func TestProgramVersion(t *testing.T) {
 	if !match {
 		t.Errorf("The expected version has not been returned")
 	}
+}
+
+func TestMainCliError(t *testing.T) {
+	oldLogFatal := logging.LogFatal
+	defer func() { logging.LogFatal = oldLogFatal }()
+	logging.LogFatal = zap.L().Panic
+	os.Args = []string{cli.AppName, "--INVALID"}
+	assert.Panics(t, main, "Expected to fail because of invalid argument name")
+}
+
+func TestMainCliExecuteError(t *testing.T) {
+	oldLogFatal := logging.LogFatal
+	defer func() { logging.LogFatal = oldLogFatal }()
+	logging.LogFatal = zap.L().Panic
+	os.Args = []string{cli.AppName, "--logLevel=INVALID"}
+	assert.Panics(t, main, "Expected to fail because of invalid argument value")
 }
