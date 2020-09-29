6b4d4ca94cbd7f3d15119472e99c2abc87f0d11c
nicolaasuni <nicola.asuni@vonage.com>
2020-09-29 12:08:18 +0100

    Fix make dbuild

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # dockerbuild.sh
 #
@@ -6,6 +6,7 @@
 #
 # @author      Nicola Asuni <info@tecnick.com>
 # ------------------------------------------------------------------------------
+set -e -u +x
 
 # NOTES:
 # This script requires Docker
@@ -17,9 +18,11 @@
 : ${CVSPATH:=project}
 : ${VENDOR:=vendor}
 : ${PROJECT:=project}
+: ${SSH_PRIVATE_KEY:=$(cat ~/.ssh/id_rsa)}
+: ${SSH_PUBLIC_KEY:=$(cat ~/.ssh/id_rsa.pub)}
 
 # make target to execute
-: ${MAKETARGET:=deps mod qa}
+: ${MAKETARGET:=deps mod qa example}
 
 # Name of the base development Docker image
 DOCKERDEV=${VENDOR}/dev_${PROJECT}
@@ -35,7 +38,24 @@ PRJPATH=/root/src/${CVSPATH}/${PROJECT}
 #       so in case of error we can continue without interrupting this script.
 cat > Dockerfile.test <<- EOM
 FROM ${DOCKERDEV}
-RUN mkdir -p ${PRJPATH}
+ARG SSH_PRIVATE_KEY=""
+ARG SSH_PUBLIC_KEY=""
+RUN \\
+mkdir -p /root/.ssh \\
+&& echo "\${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa \\
+&& echo "\${SSH_PUBLIC_KEY}" > /root/.ssh/id_rsa.pub \\
+&& echo "Host *" >> /root/.ssh/config \\
+&& echo "    StrictHostKeyChecking no" >> /root/.ssh/config \\
+&& echo "    GlobalKnownHostsFile  /dev/null" >> /root/.ssh/config \\
+&& echo "    UserKnownHostsFile    /dev/null" >> /root/.ssh/config \\
+&& chmod 600 /root/.ssh/id_rsa \\
+&& chmod 644 /root/.ssh/id_rsa.pub \\
+&& echo "[user]" >> /root/.gitconfig \\
+&& echo "	email = godev@example.com" >> /root/.gitconfig \\
+&& echo "	name = godevlocaltestuser" >> /root/.gitconfig \\
+&& echo "[url \"ssh://git@${CVSPATH}\"]" >> /root/.gitconfig \\
+&& echo "	insteadOf = https://${CVSPATH}" >> /root/.gitconfig \\
+&& mkdir -p ${PRJPATH}
 ADD ./ ${PRJPATH}
 WORKDIR ${PRJPATH}
 RUN make ${MAKETARGET} || (echo \$? > target/make.exit)
@@ -45,7 +65,7 @@ EOM
 DOCKER_IMAGE_NAME=${VENDOR}/build_${PROJECT}
 
 # Build the Docker image
-docker build --no-cache --tag ${DOCKER_IMAGE_NAME} --file Dockerfile.test .
+docker build --build-arg SSH_PRIVATE_KEY="${SSH_PRIVATE_KEY}" --build-arg SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY}" --no-cache --tag ${DOCKER_IMAGE_NAME} --file Dockerfile.test .
 
 # Start a container using the newly created Docker image
 CONTAINER_ID=$(docker run -d ${DOCKER_IMAGE_NAME})
@@ -54,5 +74,6 @@ CONTAINER_ID=$(docker run -d ${DOCKER_IMAGE_NAME})
 docker cp ${CONTAINER_ID}:"${PRJPATH}/target" ./
 
 # Remove the temporary container and image
+rm -f Dockerfile.test
 docker rm -f ${CONTAINER_ID} || true
 docker rmi -f ${DOCKER_IMAGE_NAME} || true
