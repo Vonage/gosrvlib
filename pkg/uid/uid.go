29e7252b7cc5938431fd35359ead1f231a4cde83
nicolaasuni <nicola.asuni@vonage.com>
2020-10-26 11:45:22 +0000

    Disable gosec checks for math/rand

--- pkg/uid/uid.go
+++ pkg/uid/uid.go
@@ -21,12 +21,12 @@ func InitRandSeed() error {
 // NewID64 generates and return a new 64 bit unique ID based on time (high 32 bit) and a random number (low 32 bit)
 func NewID64() string {
 	t := time.Now().UTC()
-	offset := time.Date(t.Year()-10, 1, 1, 0, 0, 0, 0, time.UTC).Unix() // [s] time starts 1st JAN 10 years ago
-	return strconv.FormatUint((((uint64)(t.Unix()-offset))<<32)+(uint64)(rand.Uint32()), 36)
+	offset := time.Date(t.Year()-10, 1, 1, 0, 0, 0, 0, time.UTC).Unix()                      // [s] time starts 1st JAN 10 years ago
+	return strconv.FormatUint((((uint64)(t.Unix()-offset))<<32)+(uint64)(rand.Uint32()), 36) // #nosec
 }
 
 // NewID128 generates and return a new 128 bit unique ID based on time (high 64 bit) and a random number (low 64 bit)
 // NOTE: works until year 2262
 func NewID128() string {
-	return strconv.FormatUint((uint64)(time.Now().UTC().UnixNano()), 36) + strconv.FormatUint(rand.Uint64(), 36)
+	return strconv.FormatUint((uint64)(time.Now().UTC().UnixNano()), 36) + strconv.FormatUint(rand.Uint64(), 36) // #nosec
 }
