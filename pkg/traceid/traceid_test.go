099aaddfc4638d22aa6cde54f633190bb7ecf7ff
nicolaasuni <nicola.asuni@vonage.com>
2020-12-01 06:31:05 +0000

    Cleanup test

--- pkg/traceid/traceid_test.go
+++ pkg/traceid/traceid_test.go
@@ -11,33 +11,31 @@ import (
 func TestToContext(t *testing.T) {
 	t.Parallel()
 
-	ctx := ToContext(context.Background(), "test-123456")
+	// store value in context
+	ctx := ToContext(context.Background(), "test-1-218549")
 
-	el1 := FromContext(ctx, "default-123456")
-	require.Equal(t, el1, "test-123456")
+	// load the value from context and ignore default
+	el1 := FromContext(ctx, "default-104173")
+	require.Equal(t, el1, "test-1-218549")
 
-	// do not override with the same id
-	ctx1 := ToContext(ctx, "test-123456")
+	// do not override the value in context
+	ctx1 := ToContext(ctx, "test-2-563011")
 	require.Equal(t, ctx, ctx1)
-
-	// do not override with other id
-	ctx2 := ToContext(ctx, "other-123456")
-	require.Equal(t, ctx, ctx2)
 }
 
 func TestFromContext(t *testing.T) {
 	t.Parallel()
 
-	// Context without request id, should return given default
-	id1 := FromContext(context.Background(), "default-12345")
+	// context without set id, should return the default value
+	id1 := FromContext(context.Background(), "default-1-206951")
 	require.NotEmpty(t, id1)
-	require.Equal(t, "default-12345", id1)
+	require.Equal(t, "default-1-206951", id1)
 
-	// Context with request id, should return the existing value
-	ctx := ToContext(context.Background(), "context-12345")
-	id2 := FromContext(ctx, "default-67890")
+	// context with set id, should return the existing value
+	ctx := ToContext(context.Background(), "default-2-616841")
+	id2 := FromContext(ctx, "default-3-67890")
 	require.NotEmpty(t, id2)
-	require.Equal(t, "context-12345", id2)
+	require.Equal(t, "default-2-616841", id2)
 }
 
 func TestToHTTPRequest(t *testing.T) {
@@ -47,19 +45,19 @@ func TestToHTTPRequest(t *testing.T) {
 	r1, err := http.NewRequest(http.MethodGet, "/", nil)
 	require.NoError(t, err)
 
-	usedFallback := ToHTTPRequest(context.Background(), r1, DefaultKey, "")
-	require.Equal(t, usedFallback, true)
-	require.Equal(t, r1.Header.Get(DefaultKey), "")
+	usedDefault1 := ToHTTPRequest(context.Background(), r1, DefaultKey, DefaultValue)
+	require.Equal(t, usedDefault1, true)
+	require.Equal(t, r1.Header.Get(DefaultKey), DefaultValue)
 
 	// header set
 	r2, err := http.NewRequest(http.MethodGet, "/", nil)
 	require.NoError(t, err)
-	ctx := ToContext(context.Background(), "test_trace_id")
+	ctx := ToContext(context.Background(), "test-904117")
 	r2 = r2.WithContext(ctx)
 
-	usedFallback2 := ToHTTPRequest(ctx, r2, DefaultKey, "")
-	require.Equal(t, usedFallback2, false)
-	require.Equal(t, "test_trace_id", r2.Header.Get(DefaultKey))
+	usedDefault2 := ToHTTPRequest(ctx, r2, DefaultKey, DefaultValue)
+	require.Equal(t, usedDefault2, false)
+	require.Equal(t, "test-904117", r2.Header.Get(DefaultKey))
 }
 
 func TestFromHTTPRequest(t *testing.T) {
@@ -69,14 +67,14 @@ func TestFromHTTPRequest(t *testing.T) {
 	r1, err := http.NewRequest(http.MethodGet, "/", nil)
 	require.NoError(t, err)
 
-	v1 := FromHTTPRequest(r1, DefaultKey, "default-123456")
-	require.Equal(t, "default-123456", v1)
+	v1 := FromHTTPRequest(r1, DefaultKey, "default-1-103993")
+	require.Equal(t, "default-1-103993", v1)
 
 	// header set should return actual value
 	r2, err := http.NewRequest(http.MethodGet, "/", nil)
 	require.NoError(t, err)
-	r2.Header.Add(DefaultKey, "reqid-1234565789")
+	r2.Header.Add(DefaultKey, "test-1-413579")
 
-	v2 := FromHTTPRequest(r2, DefaultKey, "default-123456")
-	require.Equal(t, "reqid-1234565789", v2)
+	v2 := FromHTTPRequest(r2, DefaultKey, "default-2-968041")
+	require.Equal(t, "test-1-413579", v2)
 }
