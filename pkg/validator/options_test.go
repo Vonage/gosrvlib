5e29c1d3032464c2da28c2ac03f0f5a2c60a7af6
anup-limbu <anup.limbu@vonage.com>
2021-01-08 11:57:16 +0000

    add more test

--- pkg/validator/options_test.go
+++ pkg/validator/options_test.go
@@ -1,1 +1,61 @@
 package validator
+
+import (
+	"testing"
+
+	vt "github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithDefaultTranslations(t *testing.T) {
+	tests := []struct {
+		name string
+		v    *Validator
+	}{
+		{
+			name: "success",
+			v: &Validator{
+				V: vt.New(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			err := WithDefaultTranslations()(tt.v)
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestWithFieldNameTag(t *testing.T) {
+	tests := []struct {
+		name string
+		tag  string
+	}{
+		{
+			name: "tag is empty string",
+			tag:  "",
+		},
+		{
+			name: "name with -",
+			tag:  "-abc-efgh-ijkl",
+		},
+		{
+			name: "success return name",
+			tag:  "abcderfghijk",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			v := &Validator{
+				V: vt.New(),
+			}
+			err := WithFieldNameTag(tt.tag)(v)
+			require.NoError(t, err)
+		})
+	}
+}
--- pkg/validator/validator_test.go
+++ pkg/validator/validator_test.go
@@ -3,8 +3,6 @@ package validator
 import (
 	"testing"
 
-	ut "github.com/go-playground/universal-translator"
-	vt "github.com/go-playground/validator/v10"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,50 +15,43 @@ func TestValidationError_Error(t *testing.T) {
 }
 
 func TestNew(t *testing.T) {
-	type args struct {
-		opts []Option
-	}
 	tests := []struct {
-		name           string
-		args           args
-		want           bool
-		wantErr        bool
-		wantValidate   *vt.Validate
-		wantTranslator ut.Translator
+		name    string
+		opts    []Option
+		want    bool
+		wantErr bool
 	}{
 		{
 			name:    "empty option success",
-			args:    args{nil},
+			opts:    nil,
 			want:    true,
 			wantErr: false,
 		},
 		{
 			name: "applied opts returns error",
-			args: args{
-				opts: []Option{
-					WithDefaultTranslations(),
-					WithFieldNameTag("test_tag"),
-					WithValidationTranslated("test_tag", nil, nil, nil),
-				},
+			opts: []Option{
+				WithDefaultTranslations(),
+				WithFieldNameTag("test_tag"),
+				WithValidationTranslated("test_tag", nil, nil, nil),
 			},
 			want:    false,
 			wantErr: true,
 		},
 		{
 			name: "success with opts applied",
-			args: args{
-				opts: []Option{
-					WithDefaultTranslations(),
-					WithFieldNameTag("test_tag"),
-				},
+			opts: []Option{
+				WithDefaultTranslations(),
+				WithFieldNameTag("test_tag"),
 			},
 			want:    true,
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := New(tt.args.opts...)
+			t.Parallel()
+			got, err := New(tt.opts...)
 
 			if tt.wantErr {
 				require.Nil(t, got, "New() returned Validator should be nil")
@@ -69,6 +60,57 @@ func TestNew(t *testing.T) {
 			}
 			require.NotNil(t, got, "New() returned Validator should not be nil")
 			require.NoError(t, err, "New() unexpected error = %v", err)
+		})
+	}
+}
+
+func TestValidator_ValidateStruct(t *testing.T) {
+	type baseConfig struct {
+		Base1 string `mapstructure:"base1" validate:"required,url"`
+		Base2 int    `mapstructure:"base2" validate:"required"`
+	}
+	type appConfig struct {
+		Base    baseConfig `mapstructure:"base" validate:"required"`
+		Config1 bool       `mapstructure:"config1" validate:"required"`
+		Config2 string     `mapstructure:"config2" validate:"required"`
+	}
+
+	tests := []struct {
+		name    string
+		obj     interface{}
+		wantErr bool
+	}{
+		{
+			name: "success struct validated",
+			obj: appConfig{
+				Base: baseConfig{
+					Base1: "https://test.ipify.url.invalid",
+					Base2: 1234,
+				},
+				Config1: true,
+				Config2: "test_string",
+			},
+			wantErr: false,
+		},
+		{
+			name: "failed in validation",
+			obj: appConfig{
+				Base: baseConfig{Base1: "not_url"},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			opts := []Option{
+				WithFieldNameTag("mapstructure"),
+				WithDefaultTranslations(),
+			}
+			v, _ := New(opts...)
+			err := v.ValidateStruct(tt.obj)
+			require.Equal(t, tt.wantErr, err != nil, "ValidateStruct() error = %v, wantErr %v", err, tt.wantErr)
 		})
 	}
 }
