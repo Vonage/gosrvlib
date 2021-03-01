5ea1dc2aab85983df39aa2cf9d43738464b704ab
nicolaasuni <nicola.asuni@vonage.com>
2021-03-01 17:54:39 +0000

    Add IN SQL clause functions

--- pkg/sqlutil/sqlutil.go
+++ pkg/sqlutil/sqlutil.go
@@ -2,36 +2,80 @@
 package sqlutil
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
-// BuildInClauseInt prepares a SQL IN clause with the given list of integer values.
-func BuildInClauseInt(field string, in []int) string {
-	if len(in) == 0 {
-		return ""
-	}
-
-	values := make([]string, len(in))
-
-	for i, v := range in {
-		values[i] = strconv.Itoa(v)
-	}
-
-	return "`" + field + "` IN (" + strings.Join(values, ",") + ")"
-}
+const (
+	conditionIn    = "IN"
+	conditionNotIn = "NOT IN"
+)
 
 // BuildInClauseString prepares a SQL IN clause with the given list of string values.
-func BuildInClauseString(field string, in []string) string {
-	if len(in) == 0 {
+func BuildInClauseString(field string, values []string) string {
+	return composeInClause(conditionIn, field, formatStrings(values))
+}
+
+// BuildNotInClauseString prepares a SQL NOT IN clause with the given list of string values.
+func BuildNotInClauseString(field string, values []string) string {
+	return composeInClause(conditionNotIn, field, formatStrings(values))
+}
+
+// BuildInClauseInt prepares a SQL IN clause with the given list of integer values.
+func BuildInClauseInt(field string, values []int) string {
+	return composeInClause(conditionIn, field, formatInts(values))
+}
+
+// BuildNotInClauseInt prepares a SQL NOT IN clause with the given list of integer values.
+func BuildNotInClauseInt(field string, values []int) string {
+	return composeInClause(conditionNotIn, field, formatInts(values))
+}
+
+// BuildInClauseUint prepares a SQL IN clause with the given list of integer values.
+func BuildInClauseUint(field string, values []uint64) string {
+	return composeInClause(conditionIn, field, formatUints(values))
+}
+
+// BuildNotInClauseUint prepares a SQL NOT IN clause with the given list of integer values.
+func BuildNotInClauseUint(field string, values []uint64) string {
+	return composeInClause(conditionNotIn, field, formatUints(values))
+}
+
+func formatStrings(values []string) []string {
+	items := make([]string, len(values))
+
+	for k, v := range values {
+		items[k] = "'" + v + "'"
+	}
+
+	return items
+}
+
+func formatInts(values []int) []string {
+	items := make([]string, len(values))
+
+	for k, v := range values {
+		items[k] = strconv.Itoa(v)
+	}
+
+	return items
+}
+
+func formatUints(values []uint64) []string {
+	items := make([]string, len(values))
+
+	for k, v := range values {
+		items[k] = strconv.FormatUint(v, 10)
+	}
+
+	return items
+}
+
+func composeInClause(condition string, field string, values []string) string {
+	if len(values) == 0 {
 		return ""
 	}
 
-	values := make([]string, len(in))
-
-	for k, v := range in {
-		values[k] = "'" + v + "'"
-	}
-
-	return "`" + field + "` IN (" + strings.Join(values, ",") + ")"
+	return fmt.Sprintf("`%s` %s (%s)", field, condition, strings.Join(values, ","))
 }
--- pkg/sqlutil/sqlutil_test.go
+++ pkg/sqlutil/sqlutil_test.go
@@ -6,46 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBuildInClauseInt(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name   string
-		field  string
-		values []int
-		want   string
-	}{
-		{
-			name:   "expect empty",
-			field:  "test_1",
-			values: []int{},
-			want:   "",
-		},
-		{
-			name:   "expect single value",
-			field:  "test_2",
-			values: []int{99},
-			want:   "`test_2` IN (99)",
-		},
-		{
-			name:   "expect multiple values",
-			field:  "test_3",
-			values: []int{11, 13, 17},
-			want:   "`test_3` IN (11,13,17)",
-		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			got := BuildInClauseInt(tt.field, tt.values)
-			require.Equal(t, tt.want, got)
-		})
-	}
-}
-
 func TestBuildInClauseString(t *testing.T) {
 	t.Parallel()
 
@@ -81,6 +41,206 @@ func TestBuildInClauseString(t *testing.T) {
 			t.Parallel()
 
 			got := BuildInClauseString(tt.field, tt.values)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestBuildNotInClauseString(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		field  string
+		values []string
+		want   string
+	}{
+		{
+			name:   "expect empty",
+			field:  "test_1",
+			values: []string{},
+			want:   "",
+		},
+		{
+			name:   "expect single value",
+			field:  "test_2",
+			values: []string{"A"},
+			want:   "`test_2` NOT IN ('A')",
+		},
+		{
+			name:   "expect multiple values",
+			field:  "test_3",
+			values: []string{"B", "C"},
+			want:   "`test_3` NOT IN ('B','C')",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := BuildNotInClauseString(tt.field, tt.values)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestBuildInClauseInt(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		field  string
+		values []int
+		want   string
+	}{
+		{
+			name:   "expect empty",
+			field:  "test_1",
+			values: []int{},
+			want:   "",
+		},
+		{
+			name:   "expect single value",
+			field:  "test_2",
+			values: []int{99},
+			want:   "`test_2` IN (99)",
+		},
+		{
+			name:   "expect multiple values",
+			field:  "test_3",
+			values: []int{11, -13, 17},
+			want:   "`test_3` IN (11,-13,17)",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := BuildInClauseInt(tt.field, tt.values)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestBuildNotInClauseInt(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		field  string
+		values []int
+		want   string
+	}{
+		{
+			name:   "expect empty",
+			field:  "test_1",
+			values: []int{},
+			want:   "",
+		},
+		{
+			name:   "expect single value",
+			field:  "test_2",
+			values: []int{99},
+			want:   "`test_2` NOT IN (99)",
+		},
+		{
+			name:   "expect multiple values",
+			field:  "test_3",
+			values: []int{11, -13, 17},
+			want:   "`test_3` NOT IN (11,-13,17)",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := BuildNotInClauseInt(tt.field, tt.values)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestBuildInClauseUint(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		field  string
+		values []uint64
+		want   string
+	}{
+		{
+			name:   "expect empty",
+			field:  "test_1",
+			values: []uint64{},
+			want:   "",
+		},
+		{
+			name:   "expect single value",
+			field:  "test_2",
+			values: []uint64{99},
+			want:   "`test_2` IN (99)",
+		},
+		{
+			name:   "expect multiple values",
+			field:  "test_3",
+			values: []uint64{11, 13, 17},
+			want:   "`test_3` IN (11,13,17)",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := BuildInClauseUint(tt.field, tt.values)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestBuildNotInClauseUint(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		field  string
+		values []uint64
+		want   string
+	}{
+		{
+			name:   "expect empty",
+			field:  "test_1",
+			values: []uint64{},
+			want:   "",
+		},
+		{
+			name:   "expect single value",
+			field:  "test_2",
+			values: []uint64{99},
+			want:   "`test_2` NOT IN (99)",
+		},
+		{
+			name:   "expect multiple values",
+			field:  "test_3",
+			values: []uint64{11, 13, 17},
+			want:   "`test_3` NOT IN (11,13,17)",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := BuildNotInClauseUint(tt.field, tt.values)
 			require.Equal(t, tt.want, got)
 		})
 	}
