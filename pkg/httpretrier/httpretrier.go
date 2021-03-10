980d482c05168f474ab42da8e709703329fe1939
nicolaasuni <nicola.asuni@vonage.com>
2021-03-10 19:47:13 +0000

    Add new httpretrier package - incomplete

---
+++ pkg/httpretrier/httpretrier.go
@@ -0,0 +1,129 @@
+// Package httpretrier allow to retry HTTP requests based on HTTP status boolean conditions.
+package httpretrier
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+// Boolean operations.
+const (
+	OpNotEqual = iota + 1
+	OpLessThan
+	OpLessOrEqualThan
+	OpEqual
+	OpGreaterOrEqualThan
+	OpGreaterThan
+)
+
+const (
+	// DefaultAttempts is the default maximum number of retry attempts.
+	DefaultAttempts = 3
+
+	// DefaultDelay is the default base delay amount in milliseconds.
+	DefaultDelay = 500
+
+	// DefaultDelayFactor is the default multiplication factor to get the successive delay value.
+	DefaultDelayFactor = 2
+
+	// DefaultJitter is the maximum random Jitter between retries in milliseconds.
+	DefaultJitter = 100
+)
+
+// ORGroup maps different boolean operations to a list of values to be checked against a reference value.
+type ORGroup map[int][]int
+
+// HTTPRetrier represents an instance of the HTTP retrier.
+type HTTPRetrier struct {
+	conditions  []ORGroup
+	delayFactor float64
+	delay       int64
+	jitter      int64
+	attempts    uint
+}
+
+func defaultHTTPRetrier() *HTTPRetrier {
+	return &HTTPRetrier{
+		conditions:  []ORGroup{{OpGreaterOrEqualThan: []int{500}}},
+		attempts:    DefaultAttempts,
+		delay:       DefaultDelay,
+		delayFactor: DefaultDelayFactor,
+		jitter:      DefaultJitter,
+	}
+}
+
+// New creates a new instance.
+func New(opts ...Option) (*HTTPRetrier, error) {
+	c := defaultHTTPRetrier()
+
+	for _, applyOpt := range opts {
+		if err := applyOpt(c); err != nil {
+			return nil, err
+		}
+	}
+
+	return c, nil
+}
+
+// HTTPDoFn is thr function signature of an http.Do function.
+type HTTPDoFn func(req *http.Request) (*http.Response, error)
+
+// Retry execute the Do function and retry in case of error.
+func (c *HTTPRetrier) Retry(do HTTPDoFn, req *http.Request) (*http.Response, error) {
+	delay := float64(c.delay)
+
+	for i := c.attempts; i > 1; i-- {
+		resp, err := do(req)
+		if c.check(resp.StatusCode) {
+			return resp, err
+		}
+
+		delay *= c.delayFactor
+
+		time.Sleep(time.Duration(int64(delay)+rand.Int63n(c.jitter)) * time.Millisecond) // nolint:gosec
+	}
+
+	return do(req)
+}
+
+func (c *HTTPRetrier) check(ref int) bool {
+	for _, group := range c.conditions {
+		if !checkORGroup(group, ref) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkORGroup(group ORGroup, ref int) bool {
+	for op, vals := range group {
+		for _, val := range vals {
+			if checkCondition(op, ref, val) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func checkCondition(op, ref, val int) bool {
+	switch op {
+	case OpNotEqual:
+		return ref != val
+	case OpLessThan:
+		return ref < val
+	case OpLessOrEqualThan:
+		return ref <= val
+	case OpEqual:
+		return ref == val
+	case OpGreaterOrEqualThan:
+		return ref >= val
+	case OpGreaterThan:
+		return ref > val
+	default:
+		return false
+	}
+}
---
+++ pkg/httpretrier/options.go
@@ -0,0 +1,78 @@
+package httpretrier
+
+import (
+	"fmt"
+)
+
+// Option is the interface that allows to set the options.
+type Option func(c *HTTPRetrier) error
+
+// WithStatusConditions set the boolean conditions for the HTTP status.
+func WithStatusConditions(conditions []ORGroup) Option {
+	return func(r *HTTPRetrier) error {
+		if len(conditions) == 0 {
+			return fmt.Errorf("at least one condition must be specified")
+		}
+
+		r.conditions = conditions
+
+		return nil
+	}
+}
+
+// WithAttempts set the maximum number of retries.
+func WithAttempts(attempts uint) Option {
+	return func(r *HTTPRetrier) error {
+		if attempts < 1 {
+			return fmt.Errorf("the number of attempts must be at least 1")
+		}
+
+		r.attempts = attempts
+
+		return nil
+	}
+}
+
+// WithDelay set the base delay in millisecond.
+// The base delay is always multiplied by the delay factor.
+// If the base delay is 500 milliseconds and the delay factor is 2, then the second attempt will happen after one second (2 * 500 = 1000 milliseconds).
+func WithDelay(delay int64) Option {
+	return func(r *HTTPRetrier) error {
+		if delay < 1 {
+			return fmt.Errorf("delay must be a greater than zero")
+		}
+
+		r.delay = delay
+
+		return nil
+	}
+}
+
+// WithDelayFactor set the multiplication factor to get the successive delay value.
+// A delay factor greater than 1 means an exponential delay increase.
+// if the delay factor is 2 and the base delay is 1, then the delays will be: [2, 4, 8, ...].
+func WithDelayFactor(delayFactor float64) Option {
+	return func(r *HTTPRetrier) error {
+		if delayFactor < 1 {
+			return fmt.Errorf("delay factor must be at least 1")
+		}
+
+		r.delayFactor = delayFactor
+
+		return nil
+	}
+}
+
+// WithJitter sets the maximum random Jitter in milliseconds between retries.
+// This is useful to avoid the Thundering herd problem (https://en.wikipedia.org/wiki/Thundering_herd_problem).
+func WithJitter(jitter int64) Option {
+	return func(r *HTTPRetrier) error {
+		if jitter < 1 {
+			return fmt.Errorf("jitter must be a greater than zero")
+		}
+
+		r.jitter = jitter
+
+		return nil
+	}
+}
