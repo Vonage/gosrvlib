f1b79e1348c13e824e9566946c2a79bb2e804d6e
Matteo Giaccone <matteo.giaccone@vonage.com>
2021-04-08 13:50:19 +0100

    Add sqllock package for MySQL

---
+++ pkg/sqllock/sqllock_test.go
@@ -0,0 +1,102 @@
+package database
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/nexmoinc/gosrvlib/pkg/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDB_AcquireLock(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name           string
+		setupMocks     func(mock sqlmock.Sqlmock)
+		wantErr        bool
+		wantReleaseErr bool
+	}{
+		{
+			name: "success",
+			setupMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(sqlGetLock).
+					WithArgs("key", 2).
+					WillReturnRows(sqlmock.NewRows([]string{"result"}).AddRow(1))
+
+				mock.ExpectExec(sqlReleaseLock).
+					WithArgs("key").
+					WillReturnResult(sqlmock.NewResult(0, 0))
+			},
+			wantErr: false,
+		},
+		{
+			name: "error executing get lock",
+			setupMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(sqlGetLock).
+					WillReturnError(fmt.Errorf("database error"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "error lock timeout",
+			setupMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(sqlGetLock).
+					WillReturnRows(sqlmock.NewRows([]string{"result"}).AddRow(0))
+			},
+			wantErr: true,
+		},
+		{
+			name: "error lock acquire error",
+			setupMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(sqlGetLock).
+					WillReturnRows(sqlmock.NewRows([]string{"result"}).AddRow(2))
+			},
+			wantErr: true,
+		},
+		{
+			name: "error releasing lock",
+			setupMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(sqlGetLock).
+					WillReturnRows(sqlmock.NewRows([]string{"result"}).AddRow(1))
+
+				mock.ExpectExec(sqlReleaseLock).
+					WillReturnError(fmt.Errorf("db error"))
+			},
+			wantErr:        false,
+			wantReleaseErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			require.NoError(t, err, "AcquireLock() Unexpected error while creating sqlmock", err)
+			defer func() { _ = mockDB.Close() }()
+
+			locker := New(mockDB)
+			require.NoError(t, err, "failed to create db conn")
+
+			if tt.setupMocks != nil {
+				tt.setupMocks(mock)
+			}
+
+			release, err := locker.AcquireLock(testutil.Context(), "key", 2*time.Second)
+			var releaseErr error
+
+			if release != nil {
+				releaseErr = release()
+			}
+
+			require.Equal(t, tt.wantErr, err != nil, "AcquireLock() error = %v, wantErr %v", err, tt.wantErr)
+			require.Equal(t, tt.wantReleaseErr, releaseErr != nil, "releaseLock() releaseError = %v, wantReleaseErr %v", releaseErr, tt.wantReleaseErr)
+
+			require.NoError(t, mock.ExpectationsWereMet(), "DB expectations not met")
+		})
+	}
+}
