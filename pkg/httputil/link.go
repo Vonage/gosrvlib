ddb185f6718f66fe20b906462292bc800be54a72
Corneliu Vesa <corneliu.vesa@vonage.com>
2021-05-24 16:26:28 +0100

    Add a public link builder helper

---
+++ pkg/httputil/link.go
@@ -0,0 +1,18 @@
+package httputil
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Link generates a public Link using the service url. It replaces all segments into the template.
+// The template then gets joined at the end of the service url.
+func Link(url, template string, segments ...interface{}) string {
+	template = strings.TrimLeft(template, "/")
+
+	if len(segments) > 0 {
+		template = fmt.Sprintf(template, segments...)
+	}
+
+	return url + "/" + template
+}
