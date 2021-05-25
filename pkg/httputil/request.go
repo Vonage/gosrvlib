2eec2947b151890f0d00e26f2172d97bf01ed387
nicolaasuni <nicola.asuni@vonage.com>
2021-05-25 11:10:34 +0100

    New utility functions to get integer values from HTTP request query

--- pkg/httputil/request.go
+++ pkg/httputil/request.go
@@ -3,6 +3,8 @@ package httputil
 import (
 	"encoding/base64"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
@@ -27,4 +29,22 @@ func PathParam(r *http.Request, name string) string {
 // AddBasicAuth decorates the provided http.Request with Basic Authorization.
 func AddBasicAuth(apiKey, apiSecret string, r *http.Request) {
 	r.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(apiKey+":"+apiSecret)))
+}
+
+// QueryIntOrDefault set default value for a particular int key.
+func QueryIntOrDefault(q url.Values, key string, def int) int {
+	if v, err := strconv.ParseInt(q.Get(key), 10, 64); err == nil {
+		return int(v)
+	}
+
+	return def
+}
+
+// QueryUintOrDefault set default value for a particular uint key.
+func QueryUintOrDefault(q url.Values, key string, def uint) uint {
+	if v, err := strconv.ParseUint(q.Get(key), 10, 64); err == nil {
+		return uint(v)
+	}
+
+	return def
 }
