1b3b3072264dc2839198363d4f6a71d6f181fe47
Nicola Asuni <nicolaasuni@users.noreply.github.com>
2021-07-05 15:14:37 +0100

    Add basic slack package to send messages via webhook (#110)

---
+++ pkg/slack/client_test.go
@@ -0,0 +1,265 @@
+package slack
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/nexmoinc/gosrvlib/pkg/httputil"
+	"github.com/nexmoinc/gosrvlib/pkg/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		serviceAddr string
+		opts        []Option
+		wantTimeout time.Duration
+		wantErr     bool
+	}{
+		{
+			name:        "fails with invalid character in URL",
+			serviceAddr: "http://invalid-url.domain.invalid\u007F",
+			wantErr:     true,
+		},
+		{
+			name:        "succeeds with defaults",
+			serviceAddr: "http://service.domain.invalid:1234",
+			wantTimeout: defaultRequestTimeout,
+			wantErr:     false,
+		},
+		{
+			name:        "succeeds with overridden timeout",
+			serviceAddr: "http://service.domain.invalid:1234",
+			opts:        []Option{WithRequestTimeout(2 * time.Second)},
+			wantTimeout: 2 * time.Second,
+			wantErr:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			tt.opts = append(tt.opts, WithRetryAttempts(1))
+			c, err := New(
+				tt.serviceAddr,
+				"default-username",
+				":default-iconEmoji:",
+				"https://default.iconURL.invalid",
+				"#default-channel",
+				tt.opts...,
+			)
+			if tt.wantErr {
+				require.Nil(t, c, "New() returned client should be nil")
+				require.Error(t, err, "New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			require.NotNil(t, c, "New() returned client should not be nil")
+			require.NoError(t, err, "New() unexpected error = %v", err)
+		})
+	}
+}
+
+// nolint:gocognit
+func TestClient_HealthCheck(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name                  string
+		pingHandlerDelay      time.Duration
+		pingHandlerStatusCode int
+		pingURL               string
+		pingBody              interface{}
+		wantErr               bool
+	}{
+		{
+			name:                  "returns error because of timeout",
+			pingHandlerDelay:      2 * time.Second,
+			pingHandlerStatusCode: http.StatusOK,
+			pingBody:              &status{Status: "ok"},
+			wantErr:               true,
+		},
+		{
+			name:                  "returns error from endpoint",
+			pingHandlerStatusCode: http.StatusInternalServerError,
+			pingBody:              &status{Status: "ok"},
+			wantErr:               true,
+		},
+		{
+			name:                  "fails because ping url error",
+			pingHandlerStatusCode: http.StatusOK,
+			pingURL:               "%^*&-ERROR",
+			pingBody:              &status{Status: "ok"},
+			wantErr:               true,
+		},
+		{
+			name:                  "fails because bad status",
+			pingHandlerStatusCode: http.StatusOK,
+			pingBody:              &status{Status: "error"},
+			wantErr:               true,
+		},
+		{
+			name:                  "fails because bad response body",
+			pingHandlerStatusCode: http.StatusOK,
+			pingBody:              "{",
+			wantErr:               true,
+		},
+		{
+			name:                  "returns success from endpoint",
+			pingHandlerStatusCode: http.StatusOK,
+			pingBody:              &status{Status: "ok"},
+			wantErr:               false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			mux := http.NewServeMux()
+			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				if r.Method != http.MethodGet {
+					httputil.SendStatus(r.Context(), w, http.StatusMethodNotAllowed)
+					return
+				}
+
+				if tt.pingHandlerDelay != 0 {
+					time.Sleep(tt.pingHandlerDelay)
+				}
+
+				httputil.SendJSON(r.Context(), w, tt.pingHandlerStatusCode, tt.pingBody)
+			})
+
+			ts := httptest.NewServer(mux)
+			defer ts.Close()
+
+			c, err := New(
+				ts.URL,
+				"",
+				"",
+				"",
+				"",
+				WithRetryAttempts(1),
+				WithPingURL(ts.URL),
+			)
+			require.NoError(t, err, "Client.HealthCheck() create client unexpected error = %v", err)
+
+			if tt.pingURL != "" {
+				c.pingURL = tt.pingURL
+			}
+
+			err = c.HealthCheck(testutil.Context())
+			if tt.wantErr {
+				require.Error(t, err, "Client.HealthCheck() error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				require.Nil(t, err, "Client.HealthCheck() unexpected error = %v", err)
+			}
+		})
+	}
+}
+
+func TestClient_Send(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name           string
+		webhookHandler http.HandlerFunc
+		text           string
+		username       string
+		iconEmoji      string
+		iconURL        string
+		channel        string
+		clientFunc     func(c *Client) *Client
+		wantErr        bool
+	}{
+		{
+			name: "fails because status not OK",
+			webhookHandler: func(w http.ResponseWriter, r *http.Request) {
+				httputil.SendStatus(testutil.Context(), w, http.StatusInternalServerError)
+			},
+			text:      "text 1",
+			username:  "",
+			iconEmoji: "",
+			iconURL:   "",
+			channel:   "",
+			wantErr:   true,
+		},
+		{
+			name: "fails because of timeout",
+			webhookHandler: func(w http.ResponseWriter, r *http.Request) {
+				time.Sleep(2 * time.Second)
+				httputil.SendStatus(testutil.Context(), w, http.StatusOK)
+			},
+			text:      "text TIMEOUT",
+			username:  "timeout-username",
+			iconEmoji: ":timeout-iconEmoji:",
+			iconURL:   "https://timeout.iconURL.invalid",
+			channel:   "#timeout-channel",
+			wantErr:   true,
+		},
+		{
+			name: "fails because bad address",
+			webhookHandler: func(w http.ResponseWriter, r *http.Request) {
+				httputil.SendStatus(testutil.Context(), w, http.StatusOK)
+			},
+			text:       "text address",
+			clientFunc: func(c *Client) *Client { c.address = "*&^%-ERROR-"; return c },
+			wantErr:    true,
+		},
+		{
+			name: "fails because WriteHTTPRetrier error",
+			webhookHandler: func(w http.ResponseWriter, r *http.Request) {
+				httputil.SendStatus(testutil.Context(), w, http.StatusOK)
+			},
+			text:       "text retrier",
+			clientFunc: func(c *Client) *Client { c.retryAttempts = 0; return c },
+			wantErr:    true,
+		},
+		{
+			name: "succeed with valid response",
+			webhookHandler: func(w http.ResponseWriter, r *http.Request) {
+				httputil.SendStatus(testutil.Context(), w, http.StatusOK)
+			},
+			text:      "text OK",
+			username:  "ok-username",
+			iconEmoji: ":ok-iconEmoji:",
+			iconURL:   "https://ok.iconURL.invalid",
+			channel:   "#ok-channel",
+			wantErr:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			mux := testutil.RouterWithHandler(http.MethodPost, "/", tt.webhookHandler)
+
+			ts := httptest.NewServer(mux)
+			defer ts.Close()
+
+			c, err := New(ts.URL, "default-username", ":default-iconEmoji:", "https://default.iconURL.invalid", "#default-channel")
+			require.NoError(t, err, "create client unexpected error = %v", err)
+
+			if tt.clientFunc != nil {
+				c = tt.clientFunc(c)
+			}
+
+			err = c.Send(testutil.Context(), tt.text, tt.username, tt.iconEmoji, tt.iconURL, tt.channel)
+			if tt.wantErr {
+				require.Error(t, err, "error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				require.Nil(t, err, "unexpected error = %v", err)
+			}
+		})
+	}
+}
---
+++ pkg/slack/options.go
@@ -0,0 +1,43 @@
+package slack
+
+import (
+	"time"
+)
+
+// Option is the interface that allows to set client options.
+type Option func(c *Client)
+
+// WithRequestTimeout overrides the default request timeout.
+func WithRequestTimeout(timeout time.Duration) Option {
+	return func(c *Client) {
+		c.requestTimeout = timeout
+	}
+}
+
+// WithPingTimeout overrides the default ping timeout.
+func WithPingTimeout(timeout time.Duration) Option {
+	return func(c *Client) {
+		c.pingTimeout = timeout
+	}
+}
+
+// WithPingURL overrides the default ping timeout.
+func WithPingURL(pingURL string) Option {
+	return func(c *Client) {
+		c.pingURL = pingURL
+	}
+}
+
+// WithHTTPClient overrides the default HTTP client.
+func WithHTTPClient(hc HTTPClient) Option {
+	return func(c *Client) {
+		c.httpClient = hc
+	}
+}
+
+// WithRetryAttempts overrides the default HTTP client.
+func WithRetryAttempts(attempts uint) Option {
+	return func(c *Client) {
+		c.retryAttempts = attempts
+	}
+}
