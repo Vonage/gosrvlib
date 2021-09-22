da8ab6a16ffc69eb85b20a73aaa3a08f8a9c653c
Nicola Asuni <nicolaasuni@users.noreply.github.com>
2021-09-22 12:54:03 +0100

    Update dependencies and new package for external tools (#113)
    * Update dependencies
    
    * Move external tools to a separate package

---
+++ pkg/exttools/exttools.go
@@ -0,0 +1,12 @@
+//go:build exttools
+
+// Package exttools lists external build and test tools.
+// These tools will appear in the `go.mod` file, but will not be a part of the build.
+// They will be also excluded from the binaries as the "exttools" tag is not used.
+package exttools
+
+import (
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/jstemmer/go-junit-report"
+	_ "github.com/rakyll/gotest"
+)
