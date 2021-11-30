6be7d450428720478693666bc5017a618891f5d9
nicolaasuni <nicola.asuni@vonage.com>
2021-11-30 13:59:03 +0000

    Add loadTableEnumCache test

--- pkg/enumdb/enumdb_test.go
+++ pkg/enumdb/enumdb_test.go
@@ -1,1 +1,114 @@
 package enumdb
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/nexmoinc/gosrvlib/pkg/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_loadTableEnumCache(t *testing.T) {
+	t.Parallel()
+
+	query := "SELECT `id`, `name` FROM `test_table`"
+
+	tests := []struct {
+		name      string
+		setupMock func(m sqlmock.Sqlmock)
+		wantErr   bool
+	}{
+		{
+			name: "fails prepare statement",
+			setupMock: func(m sqlmock.Sqlmock) {
+				m.ExpectPrepare(query).
+					WillReturnError(fmt.Errorf("load error"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "fails query",
+			setupMock: func(m sqlmock.Sqlmock) {
+				m.ExpectPrepare(query).
+					ExpectQuery().
+					WillReturnError(fmt.Errorf("query error"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "fails scan",
+			setupMock: func(m sqlmock.Sqlmock) {
+				rows := sqlmock.
+					NewRows([]string{"id", "name"}).
+					AddRow("wrong_type", "test_value")
+
+				m.ExpectPrepare(query).
+					ExpectQuery().
+					WillReturnRows(rows).
+					RowsWillBeClosed()
+			},
+			wantErr: true,
+		},
+		{
+			name: "fails with rows error",
+			setupMock: func(m sqlmock.Sqlmock) {
+				rows := sqlmock.
+					NewRows([]string{"id", "name"}).
+					AddRow(7, "test_value").
+					RowError(0, fmt.Errorf("row error"))
+
+				m.ExpectPrepare(query).
+					ExpectQuery().
+					WillReturnRows(rows).
+					RowsWillBeClosed()
+			},
+			wantErr: true,
+		},
+		{
+			name: "succeed loading data",
+			setupMock: func(m sqlmock.Sqlmock) {
+				rows := sqlmock.
+					NewRows([]string{"id", "name"}).
+					AddRow(1, "alpha").
+					AddRow(2, "bravo").
+					AddRow(3, "charlie")
+
+				m.ExpectPrepare(query).
+					ExpectQuery().
+					WillReturnRows(rows).
+					RowsWillBeClosed()
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			require.NoError(t, err, "Unexpected error while creating sqlmock", err)
+			defer func() { _ = mockDB.Close() }()
+
+			mock.MatchExpectationsInOrder(false)
+			if tt.setupMock != nil {
+				tt.setupMock(mock)
+			}
+
+			ec, err := loadTableEnumCache(testutil.Context(), mockDB, query)
+
+			if tt.wantErr {
+				require.Error(t, err, "an error was expected")
+			} else {
+				require.NotNil(t, ec, "the cache should not be nil")
+				id, err := ec.ID("bravo")
+				require.NoError(t, err)
+				require.Equal(t, 2, id)
+			}
+
+			require.NoError(t, mock.ExpectationsWereMet(), "DB expectations not met")
+		})
+	}
+}
