8e34a8cfb68fe1176a5605214581a6c408c31b00
nicolaasuni <nicola.asuni@vonage.com>
2022-06-22 12:31:11 +0100

    Add retrier package

---
+++ pkg/retrier/retrier.go
@@ -0,0 +1,138 @@
+// Package retrier allow to retry execute a function in case of errors.
+package retrier
+
+import (
+	"context"
+	"math/rand"
+	"time"
+)
+
+const (
+	// DefaultAttempts is the default maximum number of retry attempts.
+	DefaultAttempts = 4
+
+	// DefaultDelay is the default delay to apply after the first failed attempt.
+	DefaultDelay = 1 * time.Second
+
+	// DefaultDelayFactor is the default multiplication factor to get the successive delay value.
+	DefaultDelayFactor = 2
+
+	// DefaultJitter is the default maximum random Jitter time between retries.
+	DefaultJitter = 1 * time.Millisecond
+
+	// DefaultTimeout is the default timeout applied to each function call via context.
+	DefaultTimeout = 1 * time.Second
+)
+
+// TaskFn is the type of function to be executed.
+type TaskFn func(ctx context.Context) error
+
+// RetryIfFn is the signature of the function used to decide when retry.
+type RetryIfFn func(err error) bool
+
+// Retrier represents an instance of the HTTP retrier.
+type Retrier struct {
+	nextDelay         float64
+	delayFactor       float64
+	attempts          uint
+	remainingAttempts uint
+	delay             time.Duration
+	jitter            time.Duration
+	timeout           time.Duration
+	retryIfFn         RetryIfFn
+	timer             *time.Timer
+	resetTimer        chan time.Duration
+	ctx               context.Context
+	cancel            context.CancelFunc
+	taskError         error
+}
+
+func defaultRetrier() *Retrier {
+	return &Retrier{
+		attempts:    DefaultAttempts,
+		delay:       DefaultDelay,
+		delayFactor: DefaultDelayFactor,
+		jitter:      DefaultJitter,
+		timeout:     DefaultTimeout,
+		retryIfFn:   DefaultRetryIf,
+		resetTimer:  make(chan time.Duration, 1),
+	}
+}
+
+// DefaultRetryIf is the default function to check the retry condition.
+func DefaultRetryIf(err error) bool {
+	return err != nil
+}
+
+// New creates a new instance.
+func New(opts ...Option) (*Retrier, error) {
+	r := defaultRetrier()
+
+	for _, applyOpt := range opts {
+		if err := applyOpt(r); err != nil {
+			return nil, err
+		}
+	}
+
+	return r, nil
+}
+
+// Run attempts to execute the task according to the retry rules.
+func (r *Retrier) Run(ctx context.Context, task TaskFn) error {
+	r.nextDelay = float64(r.delay)
+	r.remainingAttempts = r.attempts
+	r.ctx, r.cancel = context.WithCancel(ctx)
+
+	r.retry(task)
+
+	return r.taskError
+}
+
+func (r *Retrier) setTimer(d time.Duration) {
+	if !r.timer.Stop() {
+		// make sure to drain timer channel before reset
+		select {
+		case <-r.timer.C:
+		default:
+		}
+	}
+
+	r.timer.Reset(d)
+}
+
+func (r *Retrier) retry(task TaskFn) {
+	defer r.cancel()
+
+	r.timer = time.NewTimer(1 * time.Nanosecond)
+
+	for {
+		select {
+		case <-r.ctx.Done():
+			r.taskError = r.ctx.Err()
+			return
+		case d := <-r.resetTimer:
+			r.setTimer(d)
+		case <-r.timer.C:
+			if r.exec(task) {
+				return
+			}
+		}
+	}
+}
+
+func (r *Retrier) exec(task TaskFn) bool {
+	ctx, cancel := context.WithTimeout(r.ctx, r.timeout)
+	r.taskError = task(ctx)
+
+	cancel()
+
+	r.remainingAttempts--
+	if r.remainingAttempts == 0 || !r.retryIfFn(r.taskError) {
+		return true
+	}
+
+	r.resetTimer <- time.Duration(int64(r.nextDelay) + rand.Int63n(int64(r.jitter))) // nolint:gosec
+	r.nextDelay *= r.delayFactor
+
+	return false
+}
---
+++ pkg/retrier/retrier_test.go
@@ -0,0 +1,172 @@
+package retrier
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		opts    []Option
+		wantErr bool
+	}{
+		{
+			name:    "succeeds with defaults",
+			wantErr: false,
+		},
+		{
+			name: "succeeds with custom values",
+			opts: []Option{
+				WithRetryIfFn(func(err error) bool { return true }),
+				WithAttempts(5),
+				WithDelay(601 * time.Millisecond),
+				WithDelayFactor(1.3),
+				WithJitter(109 * time.Millisecond),
+				WithTimeout(131 * time.Millisecond),
+			},
+			wantErr: false,
+		},
+		{
+			name:    "fails with invalid option",
+			opts:    []Option{WithJitter(0)},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			r, err := New(tt.opts...)
+			if tt.wantErr {
+				require.Nil(t, r)
+				require.Error(t, err, "New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			require.NotNil(t, r, "New() returned value should not be nil")
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestRetrier_Run(t *testing.T) {
+	t.Parallel()
+
+	var count int
+
+	tests := []struct {
+		name                  string
+		task                  TaskFn
+		timeout               time.Duration
+		wantRemainingAttempts uint
+		wantErr               bool
+	}{
+		{
+			name:                  "success at first attempt",
+			task:                  func(_ context.Context) error { return nil },
+			timeout:               1 * time.Second,
+			wantRemainingAttempts: 3,
+		},
+		{
+			name: "success at third attempt",
+			task: func(_ context.Context) error {
+				if count == 2 {
+					return nil
+				}
+				count++
+				return fmt.Errorf("ERROR")
+			},
+			timeout:               1 * time.Second,
+			wantRemainingAttempts: 1,
+		},
+		{
+			name:                  "fail all attempts",
+			task:                  func(_ context.Context) error { return fmt.Errorf("ERROR") },
+			timeout:               1 * time.Second,
+			wantRemainingAttempts: 0,
+			wantErr:               true,
+		},
+		{
+			name:                  "fail with main timeout",
+			task:                  func(ctx context.Context) error { <-ctx.Done(); return fmt.Errorf("ERROR") },
+			timeout:               1 * time.Millisecond,
+			wantRemainingAttempts: 3,
+			wantErr:               true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			opts := []Option{
+				WithRetryIfFn(DefaultRetryIf),
+				WithAttempts(4),
+				WithDelay(10 * time.Millisecond),
+				WithDelayFactor(1.1),
+				WithJitter(5 * time.Millisecond),
+				WithTimeout(2 * time.Millisecond),
+			}
+
+			r, err := New(opts...)
+			require.NoError(t, err)
+			require.NotNil(t, r)
+
+			ctx, cancel := context.WithTimeout(context.TODO(), tt.timeout)
+			defer cancel()
+
+			err = r.Run(ctx, tt.task)
+			require.Equal(t, tt.wantErr, err != nil, "Run() error = %v, wantErr %v", err, tt.wantErr)
+			require.Equal(t, tt.wantRemainingAttempts, r.remainingAttempts, "Run() remainingAttempts = %v, wantRemainingAttempts %v", err, tt.wantErr)
+		})
+	}
+}
+
+func TestDefaultRetryIf(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		err  error
+		want bool
+	}{
+		{
+			name: "true with error",
+			err:  fmt.Errorf("ERROR"),
+			want: true,
+		},
+		{
+			name: "false with no error",
+			err:  nil,
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := DefaultRetryIf(tt.err)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestRetrier_setTimer(t *testing.T) {
+	t.Parallel()
+
+	r := &Retrier{
+		timer: time.NewTimer(1 * time.Millisecond),
+	}
+
+	time.Sleep(2 * time.Millisecond)
+	r.setTimer(2 * time.Millisecond)
+	<-r.timer.C
+}
