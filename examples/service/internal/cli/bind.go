6d9d383edc418b11fb7bd962506e6f2125397ae5
nicolaasuni <nicola.asuni@vonage.com>
2022-08-10 09:11:21 +0100

    Improve example service bind

--- examples/service/internal/cli/bind.go
+++ examples/service/internal/cli/bind.go
@@ -3,13 +3,11 @@ package cli
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/gosrvlibexample/gosrvlibexample/internal/httphandler"
 	instr "github.com/gosrvlibexample/gosrvlibexample/internal/metrics"
 	"github.com/nexmoinc/gosrvlib/pkg/bootstrap"
-	"github.com/nexmoinc/gosrvlib/pkg/errtrace"
 	"github.com/nexmoinc/gosrvlib/pkg/healthcheck"
 	"github.com/nexmoinc/gosrvlib/pkg/httpclient"
 	"github.com/nexmoinc/gosrvlib/pkg/httpserver"
@@ -20,21 +18,30 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	httpClientTimeout = 1 * time.Minute
+)
+
 // bind is the entry point of the service, this is where the wiring of all components happens.
 func bind(cfg *appConfig, appInfo *jsendx.AppInfo, mtr instr.Metrics) bootstrap.BindFunc {
 	return func(ctx context.Context, l *zap.Logger, m metrics.Client) error {
-		var statusHandler http.HandlerFunc
-
 		// We assume the service is disabled and override the service binder if required
 		serviceBinder := httpserver.NopBinder()
+		statusHandler := jsendx.DefaultStatusHandler(appInfo)
+
+		// common HTTP client used for all outbound requests
+		httpClient := httpclient.New(
+			httpclient.WithTimeout(httpClientTimeout),
+			httpclient.WithRoundTripper(m.InstrumentRoundTripper),
+			httpclient.WithTraceIDHeaderName(traceid.DefaultHeader),
+			httpclient.WithComponent(appInfo.ProgramName),
+		)
+
 		if cfg.Enabled {
-			// NOTE: Add initialization and wiring of external components and service here
-			//
-			// <INIT CODE HERE>
-			//
+			// wire the binder
 			serviceBinder = httphandler.New(nil)
 
-			// Custom healthcheck handler with JSendX response
+			// override the default healthcheck handler
 			healthCheckHandler := healthcheck.NewHandler(
 				[]healthcheck.HealthCheck{
 					// healthcheck.New("<ID>", < HANDLER >),
@@ -43,26 +50,16 @@ func bind(cfg *appConfig, appInfo *jsendx.AppInfo, mtr instr.Metrics) bootstrap.
 				healthcheck.WithResultWriter(jsendx.HealthCheckResultWriter(appInfo)),
 			)
 			statusHandler = healthCheckHandler.ServeHTTP
-		} else {
-			statusHandler = jsendx.DefaultStatusHandler(appInfo)
 		}
 
 		// ipify client
-		ipcTimeout := time.Duration(cfg.Ipify.Timeout) * time.Second
-		ipc := httpclient.New(
-			httpclient.WithTimeout(ipcTimeout),
-			httpclient.WithRoundTripper(m.InstrumentRoundTripper),
-			httpclient.WithTraceIDHeaderName(traceid.DefaultHeader),
-			httpclient.WithComponent("ipify"),
-		)
-
 		ipifyClient, err := ipify.New(
-			ipify.WithHTTPClient(ipc),
-			ipify.WithTimeout(ipcTimeout),
+			ipify.WithHTTPClient(httpClient),
+			ipify.WithTimeout(time.Duration(cfg.Ipify.Timeout)*time.Second),
 			ipify.WithURL(cfg.Ipify.Address),
 		)
 		if err != nil {
-			return fmt.Errorf("failed to build ipify client: %w", errtrace.Trace(err))
+			return fmt.Errorf("failed to build ipify client: %w", err)
 		}
 
 		// start monitoring server
@@ -70,8 +67,9 @@ func bind(cfg *appConfig, appInfo *jsendx.AppInfo, mtr instr.Metrics) bootstrap.
 			httpserver.WithServerAddr(cfg.MonitoringAddress),
 			httpserver.WithMetricsHandlerFunc(m.MetricsHandlerFunc()),
 			httpserver.WithInstrumentHandler(m.InstrumentHandler),
-			httpserver.WithEnableAllDefaultRoutes(),
+			httpserver.WithTraceIDHeaderName(traceid.DefaultHeader),
 			httpserver.WithRouter(jsendx.NewRouter(appInfo, m.InstrumentHandler)), // set default 404, 405 and panic handlers
+			httpserver.WithEnableAllDefaultRoutes(),
 			httpserver.WithIndexHandlerFunc(jsendx.DefaultIndexHandler(appInfo)),
 			httpserver.WithIPHandlerFunc(jsendx.DefaultIPHandler(appInfo, ipifyClient.GetPublicIP)),
 			httpserver.WithPingHandlerFunc(jsendx.DefaultPingHandler(appInfo)),
@@ -79,20 +77,22 @@ func bind(cfg *appConfig, appInfo *jsendx.AppInfo, mtr instr.Metrics) bootstrap.
 		}
 
 		if err := httpserver.Start(ctx, httpserver.NopBinder(), httpMonitoringOpts...); err != nil {
-			return fmt.Errorf("error starting monitoring HTTP server: %w", errtrace.Trace(err))
+			return fmt.Errorf("error starting monitoring HTTP server: %w", err)
 		}
 
 		// example of custom metric
 		mtr.IncExampleCounter("START")
 
-		// start service server
-		httpServiceOpts := []httpserver.Option{
+		// start public server
+		httpPublicOpts := []httpserver.Option{
 			httpserver.WithServerAddr(cfg.PublicAddress),
+			httpserver.WithInstrumentHandler(m.InstrumentHandler),
+			httpserver.WithTraceIDHeaderName(traceid.DefaultHeader),
 			httpserver.WithEnableDefaultRoutes(httpserver.PingRoute),
 		}
 
-		if err := httpserver.Start(ctx, serviceBinder, httpServiceOpts...); err != nil {
-			return fmt.Errorf("error starting service HTTP server: %w", errtrace.Trace(err))
+		if err := httpserver.Start(ctx, serviceBinder, httpPublicOpts...); err != nil {
+			return fmt.Errorf("error starting public HTTP server: %w", err)
 		}
 
 		return nil
