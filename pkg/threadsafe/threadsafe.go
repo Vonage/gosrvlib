c001af64b17cd9738ac161aca733848448d3569b
nicolaasuni <nicola.asuni@vonage.com>
2022-09-05 09:36:23 +0100

    Add new threadsafe package

---
+++ pkg/threadsafe/threadsafe.go
@@ -0,0 +1,14 @@
+// Package threadsafe provides a collection of thread-safe functions that can be safely used between multiple goroutines.
+package threadsafe
+
+import (
+	"sync"
+)
+
+// Append is a thread-safe version of the Go built-in append function.
+func Append[T any](mux sync.Locker, slice *[]T, item T) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	*slice = append(*slice, item)
+}
---
+++ pkg/threadsafe/threadsafe_test.go
@@ -0,0 +1,44 @@
+package threadsafe
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAppend_simple(t *testing.T) {
+	t.Parallel()
+
+	mux := &sync.Mutex{}
+
+	slice := make([]string, 0, 2)
+	Append(mux, &slice, "Hello")
+	Append(mux, &slice, "World")
+
+	require.ElementsMatch(t, []string{"Hello", "World"}, slice)
+}
+
+func TestAppend_concurrent(t *testing.T) {
+	t.Parallel()
+
+	wg := &sync.WaitGroup{}
+	mux := &sync.RWMutex{}
+
+	max := 5
+	slice := make([]int, 0, max)
+
+	for i := 0; i < max; i++ {
+		wg.Add(1)
+
+		go func(item int) {
+			defer wg.Done()
+
+			Append(mux, &slice, item)
+		}(i)
+	}
+
+	wg.Wait()
+
+	require.ElementsMatch(t, []int{0, 1, 2, 3, 4}, slice)
+}
