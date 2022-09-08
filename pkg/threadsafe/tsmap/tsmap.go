0580ad366cfb8cb1de6d00ab60a62abe40a4f7c6
Nicola Asuni <nicolaasuni@users.noreply.github.com>
2022-09-08 17:21:48 +0100

    New tsslice and ts map packages (#146)

---
+++ pkg/threadsafe/tsmap/tsmap.go
@@ -0,0 +1,30 @@
+// Package tsmap provides a collection of thread-safe map functions that can be safely used between multiple goroutines.
+package tsmap
+
+import (
+	"github.com/nexmoinc/gosrvlib/pkg/threadsafe"
+)
+
+// Set is a thread-safe function to assign a value to a key in a map.
+func Set[K comparable, V any](mux threadsafe.Locker, m map[K]V, key K, value V) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	m[key] = value
+}
+
+// Get is a thread-safe function to get a value by key in a map.
+func Get[K comparable, V any](mux threadsafe.RLocker, m map[K]V, key K) V {
+	mux.RLock()
+	defer mux.RUnlock()
+
+	return m[key]
+}
+
+// Len is a thread-safe function to get the length of a map.
+func Len[K comparable, V any](mux threadsafe.RLocker, m map[K]V) int {
+	mux.RLock()
+	defer mux.RUnlock()
+
+	return len(m)
+}
