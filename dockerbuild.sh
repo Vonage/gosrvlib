8ca976bb1917841d52e63c6d2d4e061f927cda53
nicolaasuni-vonage <nicola.asuni+github@vonage.com>
2023-05-04 13:58:30 +0100

    Generalize dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -14,25 +14,27 @@ set -e -u +x
 # EXAMPLE USAGE:
 # CVSPATH=project VENDOR=vendorname PROJECT=projectname MAKETARGET=buildall ./dockerbuild.sh
 
-# Get vendor and project name
+# Variables (parameters).
 : ${CVSPATH:=project}
 : ${VENDOR:=vendor}
 : ${PROJECT:=project}
+: ${MAKETARGET:=format clean mod deps generate qa example}
 : ${SSH_PRIVATE_KEY:=$(cat ~/.ssh/id_rsa || cat ~/.ssh/id_ed25519)}
 : ${SSH_PUBLIC_KEY:=$(cat ~/.ssh/id_rsa.pub || cat ~/.ssh/id_ed25519.pub)}
 : ${DOCKER:=$(which docker)}
+: ${DOCKERDEV:=${VENDOR}/dev_${PROJECT}}
 
-# make target to execute
-: ${MAKETARGET:=format clean mod deps generate qa example}
+# Build the base environment and keep it cached locally.
+${DOCKER} build --pull --tag ${DOCKERDEV} --file ./resources/docker/Dockerfile.dev ./resources/docker/
 
-# Define the project root path
+# Define the project root path.
 PRJPATH=/root/src/${CVSPATH}/${PROJECT}
 
 # Generate a temporary Dockerfile to build and test the project
 # NOTE: The exit status of the RUN command is stored to be returned later,
 #       so in case of error we can continue without interrupting this script.
 cat > Dockerfile.test <<- EOM
-FROM ${VENDOR}/dev_${PROJECT}
+FROM ${DOCKERDEV}
 ARG SSH_PRIVATE_KEY=""
 ARG SSH_PUBLIC_KEY=""
 RUN \\
@@ -56,19 +58,25 @@ WORKDIR ${PRJPATH}
 RUN make ${MAKETARGET} || (echo \$? > target/make.exit)
 EOM
 
-# Define the temporary Docker image name
+# Define the temporary Docker image name.
 DOCKER_IMAGE_NAME=${VENDOR}/build_${PROJECT}
 
-# Build the Docker image
-BUILDKIT_PROGRESS=plain ${DOCKER} build --no-cache --build-arg SSH_PRIVATE_KEY="${SSH_PRIVATE_KEY}" --build-arg SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY}" --tag ${DOCKER_IMAGE_NAME} --file Dockerfile.test .
+# Build the Docker image.
+BUILDKIT_PROGRESS=plain \
+${DOCKER} build \
+--no-cache \
+--build-arg SSH_PRIVATE_KEY="${SSH_PRIVATE_KEY}" \
+--build-arg SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY}" \
+--tag ${DOCKER_IMAGE_NAME} \
+--file Dockerfile.test .
 
-# Start a container using the newly created Docker image
+# Start a container using the newly created Docker image.
 CONTAINER_ID=$(docker run -d ${DOCKER_IMAGE_NAME})
 
-# Copy all build/test artifacts back to the host
+# Copy all build/test artifacts back to the host.
 ${DOCKER} cp ${CONTAINER_ID}:"${PRJPATH}/target" ./
 
-# Remove the temporary container and image
+# Remove the temporary container and image.
 rm -f Dockerfile.test
 ${DOCKER} rm -f ${CONTAINER_ID} || true
 ${DOCKER} rmi -f ${DOCKER_IMAGE_NAME} || true
