d67c6c8c8cac42905bc5df7815f464cb9173d497
nicolaasuni-vonage <nicola.asuni@vonage.com>
2023-09-17 15:56:04 +0100

    Split Stats function and fix Entropy calculation.

--- pkg/sliceutil/stats_test.go
+++ pkg/sliceutil/stats_test.go
@@ -43,37 +43,37 @@ func TestStats(t *testing.T) {
 		{
 			name:    "Two elements",
 			data:    []int{29, 13},
-			want:    &DescStats[int]{Count: 2, Entropy: -33.3443, ExKurtosis: 0, Max: 29, MaxID: 0, Mean: 21, MeanDev: 0, Median: 21, Min: 13, MinID: 1, Mode: 29, ModeFreq: 1, Range: 16, Skewness: 0, StdDev: 11.3137, Sum: 42, Variance: 128},
+			want:    &DescStats[int]{Count: 2, Entropy: 0.6187, ExKurtosis: 0, Max: 29, MaxID: 0, Mean: 21, MeanDev: 0, Median: 21, Min: 13, MinID: 1, Mode: 29, ModeFreq: 1, Range: 16, Skewness: 0, StdDev: 11.3137, Sum: 42, Variance: 128},
 			wantErr: false,
 		},
 		{
 			name:    "Three elements",
 			data:    []int{13, 37, 29},
-			want:    &DescStats[int]{Count: 3, Entropy: -231.2555, ExKurtosis: 0, Max: 37, MaxID: 1, Mean: 26.3333, MeanDev: 1.1842e-15, Median: 29, Min: 13, MinID: 0, Mode: 13, ModeFreq: 1, Range: 24, Skewness: -0.9352, StdDev: 12.2202, Sum: 79, Variance: 149.3333},
+			want:    &DescStats[int]{Count: 3, Entropy: 1.0200, ExKurtosis: 0, Max: 37, MaxID: 1, Mean: 26.3333, MeanDev: 1.1842e-15, Median: 29, Min: 13, MinID: 0, Mode: 13, ModeFreq: 1, Range: 24, Skewness: -0.9352, StdDev: 12.2202, Sum: 79, Variance: 149.3333},
 			wantErr: false,
 		},
 		{
 			name:    "Four elements",
 			data:    []int{53, 13, 37, 29},
-			want:    &DescStats[int]{Count: 4, Entropy: -264.5998, ExKurtosis: 0.3905, Max: 53, MaxID: 0, Mean: 33, MeanDev: 0, Median: 33, Min: 13, MinID: 1, Mode: 53, ModeFreq: 1, Range: 40, Skewness: 0, StdDev: 16.6533, Sum: 132, Variance: 277.3333},
+			want:    &DescStats[int]{Count: 4, Entropy: 1.2841, ExKurtosis: 0.3905, Max: 53, MaxID: 0, Mean: 33, MeanDev: 0, Median: 33, Min: 13, MinID: 1, Mode: 53, ModeFreq: 1, Range: 40, Skewness: 0, StdDev: 16.6533, Sum: 132, Variance: 277.3333},
 			wantErr: false,
 		},
 		{
 			name:    "Five elements",
 			data:    []int{53, 13, 79, 37, 29},
-			want:    &DescStats[int]{Count: 5, Entropy: -609.7862, ExKurtosis: 0.1751, Max: 79, MaxID: 2, Mean: 42.2, MeanDev: -2.8421e-15, Median: 37, Min: 13, MinID: 1, Mode: 53, ModeFreq: 1, Range: 66, Skewness: 0.6242, StdDev: 25.1236, Sum: 211, Variance: 631.2},
+			want:    &DescStats[int]{Count: 5, Entropy: 1.4645, ExKurtosis: 0.1751, Max: 79, MaxID: 2, Mean: 42.2, MeanDev: -2.8421e-15, Median: 37, Min: 13, MinID: 1, Mode: 53, ModeFreq: 1, Range: 66, Skewness: 0.6242, StdDev: 25.1236, Sum: 211, Variance: 631.2},
 			wantErr: false,
 		},
 		{
 			name:    "Six elements",
 			data:    []int{53, 83, 13, 79, 37, 29},
-			want:    &DescStats[int]{Count: 6, Entropy: -976.5500, ExKurtosis: -1.6680, Max: 83, MaxID: 1, Mean: 49, MeanDev: 0, Median: 45, Min: 13, MinID: 2, Mode: 53, ModeFreq: 1, Range: 70, Skewness: 0.1368, StdDev: 27.9714, Sum: 294, Variance: 782.4},
+			want:    &DescStats[int]{Count: 6, Entropy: 1.6462, ExKurtosis: -1.6680, Max: 83, MaxID: 1, Mean: 49, MeanDev: 0, Median: 45, Min: 13, MinID: 2, Mode: 53, ModeFreq: 1, Range: 70, Skewness: 0.1368, StdDev: 27.9714, Sum: 294, Variance: 782.4},
 			wantErr: false,
 		},
 		{
 			name:    "General case",
 			data:    []int{53, 83, 13, 79, 13, 37, 83, 29, 37, 13, 83, 83},
-			want:    &DescStats[int]{Count: 12, Entropy: -2277.1339, ExKurtosis: -1.9100, Max: 83, MaxID: 1, Mean: 50.5, MeanDev: 0, Median: 45, Min: 13, MinID: 2, Mode: 83, ModeFreq: 4, Range: 70, Skewness: -0.0494, StdDev: 30.2850, Sum: 606, Variance: 917.1818},
+			want:    &DescStats[int]{Count: 12, Entropy: 2.3019, ExKurtosis: -1.9100, Max: 83, MaxID: 1, Mean: 50.5, MeanDev: 0, Median: 45, Min: 13, MinID: 2, Mode: 83, ModeFreq: 4, Range: 70, Skewness: -0.0494, StdDev: 30.2850, Sum: 606, Variance: 917.1818},
 			wantErr: false,
 		},
 	}
