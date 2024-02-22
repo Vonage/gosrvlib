ade6c3233af6b4b2631b13f736a68437baa6e1a4
nicolaasuni-vonage <nicola.asuni@vonage.com>
2024-02-22 10:48:11 +0000

    Use new math/rand/v2 package and remove deprecation

--- pkg/uid/uid.go
+++ pkg/uid/uid.go
@@ -3,28 +3,28 @@ Package uid provides functions to generate simple time-and-random-based unique i
 
 It provides functions to generate 64 and 128 bit random identifiers in base-36 string format.
 
-Deprecated: use github.com/Vonage/gosrvlib/pkg/uidc package instead.
+This package's outputs might be easily predictable.
+For random IDs suitable for security-sensitive work, please the uidc package instead.
 */
 package uid
 
-import "github.com/Vonage/gosrvlib/pkg/uidc"
-
-// InitRandSeed initialize the random generator seed.
-// You have to call this first and only once.
-// Deprecated: use github.com/Vonage/gosrvlib/pkg/uidc package instead.
-func InitRandSeed() error {
-	return nil
-}
+//nolint:gci
+import (
+	"math/rand/v2"
+	"strconv"
+	"time"
+)
 
 // NewID64 generates and returns a new base-36-string-formatted 64 bit unique ID based on time (high 32 bit) and a random number (low 32 bit).
 // NOTE: the zero time is set to the 1st of January of 10 years ago.
-// Deprecated: use github.com/Vonage/gosrvlib/pkg/uidc package instead.
 func NewID64() string {
-	return uidc.NewID64()
+	t := time.Now().UTC()
+	offset := time.Date(t.Year()-10, 1, 1, 0, 0, 0, 0, time.UTC).Unix() // [s] time starts 1st JAN 10 years ago
+
+	return strconv.FormatUint((((uint64)(t.Unix()-offset))<<32)+(uint64)(rand.Uint32()), 36) // #nosec
 }
 
 // NewID128 generates and returns a new base-36-string-formatted 128 bit unique ID based on time (high 64 bit) and a random number (low 64 bit).
-// Deprecated: use github.com/Vonage/gosrvlib/pkg/uidc package instead.
 func NewID128() string {
-	return uidc.NewID128()
+	return strconv.FormatUint((uint64)(time.Now().UTC().UnixNano()), 36) + strconv.FormatUint(rand.Uint64(), 36) // #nosec
 }
